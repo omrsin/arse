0fb50ec265b0640684921dac699bcfd22233302a
Estee <sti.diro@gmail.com>
2015-11-04 17:05:16 +0100

    Added add story, still not working

--- ARSE/client/app/pbi/pbi.controller.js
+++ ARSE/client/app/pbi/pbi.controller.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('arseApp')
-  .controller('PbiCtrl', function ($scope) {
+  .controller('PbiCtrl', ['$scope', 'Story', function($scope, Story) {
     $scope.message = 'Hello';
     $scope.createStory = function(form) {
       console.log($scope.name);
       console.log($scope.description);
       console.log($scope.points);
       console.log($scope.summary);
+      $scope.story = new Story({name:$scope.name, project:"563a2b49fbc778f12656a4cf", description: $scope.description, points: $scope.points, summary: $scope.summary})
+      $scope.story.$save();
     }
-  });
+  }]);
