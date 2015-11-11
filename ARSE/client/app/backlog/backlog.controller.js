767a68da3e1e517726889c287ae2dcd2b56e4916
Ilmi Ali <ilmiali@gmail.com>
2015-11-11 08:15:42 +0100

    Fixed some reordering issues

--- ARSE/client/app/backlog/backlog.controller.js
+++ ARSE/client/app/backlog/backlog.controller.js
@@ -1,13 +1,24 @@
 'use strict';
 
 angular.module('arseApp')
-  .controller('BacklogCtrl', ['$scope', 'Project', '$http', function ($scope, Project, $http) {
+  .controller('BacklogCtrl', ['$scope', 'Project', '$http', '$uibModal', '$modalInstance', function ($scope, Project, $http, $uibModal, $modalInstance) {
     $scope.data = {};
     $scope.stories = [];
     $scope.project_id = "56407df1696d89ff090d9de7";
     Project.get({ id: $scope.project_id }, function (project) {
       $scope.stories = project.stories;
     });
+    
+    $scope.addStory = function () {
+      
+      var modalInstance = $uibModal.open( {
+        animation: true,
+        templateUrl: 'backlog.html',
+        controller: 'BacklogCtrl'
+
+      });
+      
+    }
 
     $scope.$on('updateView', function () {
 
