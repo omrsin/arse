15f47019b4a79f2842e51f9bc7c66c04edb34815
Omar Erminy <omrsin@gmail.com>
2015-11-18 15:18:57 +0100

    Fixes the way the rows in the projects list are clicked

--- ARSE/client/app/project/project.js
+++ ARSE/client/app/project/project.js
@@ -3,12 +3,12 @@
 angular.module('arseApp')
   .config(function ($stateProvider) {
     $stateProvider
-      .state('projects', {
+      .state('project', {
         url: '/projects',
         templateUrl: 'app/project/project.html',
         controller: 'ProjectCtrl'
       })
-      .state('projects.new', {
+      .state('project.new', {
         url: '/new',
         templateUrl: 'app/project/new.html',
         controller: 'ProjectCtrl'
