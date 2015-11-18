ca7d68ee05c9c89fb1acccdb55fcb010db94e595
Omar Erminy <omrsin@gmail.com>
2015-11-18 14:53:49 +0100

    Adds projects via a modal window, it does not refresh the list of models yet

--- ARSE/client/app/project/project.controller.js
+++ ARSE/client/app/project/project.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('arseApp')
-  .controller('ProjectCtrl', ['$scope', 'Project', '$state', function ($scope, Project, $state) {
-
+  .controller('ProjectCtrl', ['$scope', '$state', 'Project', 'Modal', function ($scope, $state, Project, Modal) {
+    
     $scope.projects = [];
 
     Project.query(function (projects) {
@@ -10,19 +10,36 @@ angular.module('arseApp')
     });
 
     $scope.$on('updateView', function () {
-
       Project.query(function (projects) {
         $scope.projects = projects;
       });
-
     });
 
-    $scope.newProject = function () {
-      console.log("Add Project");
+    $scope.new = function () {
+      console.log("New");
+      $scope.project_id = "Bitch";
+      $scope.modal = Modal.open({}, 'app/project/new.html', 'ProjectModalCtrl', $scope.project_id);      
     };
 
-    $scope.showProject = function(project){
-      $state.go("backlog", { project_id: project._id })
+    $scope.show = function(project){
+      $state.go("backlog", { project_id: project._id });
+    };
+  }])
+  .controller('ProjectModalCtrl', ['$scope', '$uibModalInstance', 'items', 'Project', '$rootScope', function($scope, $uibModalInstance, items, Project, $rootScope) {
+
+    $scope.create = function () {      
+      if ($scope.name && $scope.description) {
+        var newProject = new Project({ name: $scope.name, description: $scope.description });
+        newProject.$save(function (res) {
+          console.log(res);
+          $uibModalInstance.close();
+        });
+      } else {
+        //TODO error message
+      }
     };
 
+    $scope.cancel = function(){      
+      $uibModalInstance.dismiss();
+    };
   }]);
--- ARSE/client/app/project/project.js
+++ ARSE/client/app/project/project.js
@@ -7,5 +7,10 @@ angular.module('arseApp')
         url: '/projects',
         templateUrl: 'app/project/project.html',
         controller: 'ProjectCtrl'
+      })
+      .state('projects.new', {
+        url: '/new',
+        templateUrl: 'app/project/new.html',
+        controller: 'ProjectCtrl'
       });
   });
