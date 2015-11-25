c1b3370c085c6ff50c7716ac2c15cc4e1f3f07df
Satia Herfert <satiaherfert@gmx.de>
2015-11-25 17:09:39 +0100

    StoryDetail directive that will show details about one story in the future.

---
+++ ARSE/client/app/directives/storyDetail/storyDetail.directive.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('arseApp')
+  .directive('storyDetail', function () {
+    return {
+      templateUrl: 'app/directives/storyDetail/storyDetail.html',
+      restrict: 'E',
+      scope: {
+        storyItem:'@'
+      },
+      link: function (scope, element, attrs) {
+        console.log(scope.detailStory);
+        // storyItem is fucking JSON STRING - needs to be converted back to object -.-
+        attrs.$observe('storyItem', function(value){
+          if(value){
+              scope.item = angular.fromJson(attrs.storyItem);
+          }
+        });
+      }
+    };
+  });
