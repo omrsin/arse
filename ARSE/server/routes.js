e0e70141ef9cdb188bc2dbbdafd2fb5ccb20321a
Satia Herfert <satiaherfert@gmx.de>
2015-12-09 14:52:54 +0100

    Removed 'thing' from the server

--- ARSE/server/routes.js
+++ ARSE/server/routes.js
@@ -18,7 +18,6 @@ module.exports = function(app) {
 
 
   app.use('/api/projects', require('./api/project'));
-  app.use('/api/things', require('./api/thing'));
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
