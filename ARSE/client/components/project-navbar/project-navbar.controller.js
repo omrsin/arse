95d35eb981e4ed7c06e0bbfbdb52e2824c691b80
Satia Herfert <satiaherfert@gmx.de>
2015-12-13 12:01:42 +0100

    Disabled access to the Sprint Board if no Sprint is running.

--- ARSE/client/components/project-navbar/project-navbar.controller.js
+++ ARSE/client/components/project-navbar/project-navbar.controller.js
@@ -3,18 +3,23 @@
 angular.module('arseApp')
   .controller('ProjectNavbarCtrl', ['$scope', '$stateParams', '$state', "Project", function ($scope, $stateParams, $state, Project) {
     
-    var project;
 
-    project = Project.get({ id: $stateParams.project_id });
+    Project.get({ id: $stateParams.project_id }, function (p) {
+      $scope.project = p;
+      console.log("Project:")
+      console.log($scope.project);
+    });
 
     $scope.showProductBacklog = function(){      
       console.log("This is working");
-      $state.go("backlog", { project_id: project._id });
+      $state.go("backlog", { project_id: $scope.project._id });
     };
 
     $scope.showSprintBoard = function(){
-      console.log("Entered there");
-      $state.go("sprintBoard", { project_id: $stateParams.project_id, sprint_id: project.current_sprint });
+      if($scope.project.current_sprint) {
+        console.log("Entered there");
+        $state.go("sprintBoard", { project_id: $stateParams.project_id, sprint_id: $scope.project.current_sprint });
+      }
     };
 
   }]);
