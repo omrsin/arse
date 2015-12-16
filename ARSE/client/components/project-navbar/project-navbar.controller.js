572ef6161f980f1abd5c6d2d282f4dcd8c277877
Ment <klinakuf@gmail.com>
2015-12-16 10:11:47 +0100

    Cleaned comments working code

--- ARSE/client/components/project-navbar/project-navbar.controller.js
+++ ARSE/client/components/project-navbar/project-navbar.controller.js
@@ -11,13 +11,6 @@ angular.module('arseApp')
     });
 
     $scope.showProductBacklog = function(){      
-// <<<<<<< HEAD
-//       $state.go("backlog", { project_id: project._id });
-//     };
-// 
-//     $scope.showSprintBoard = function(){
-//       $state.go("sprintBoard", { project_id: $stateParams.project_id, sprint_id: project.current_sprint });
-// =======
       console.log("This is working");
       $state.go("backlog", { project_id: $scope.nav_project._id });
     };
@@ -27,7 +20,6 @@ angular.module('arseApp')
         console.log("Entered there");
         $state.go("sprintBoard", { project_id: $stateParams.project_id, sprint_id: $scope.nav_project.current_sprint });
       }
-// >>>>>>> d9dd279d3d6731010fae81cbf862a654b0c17d11
     };
 
     $scope.showUserManagement = function(){
