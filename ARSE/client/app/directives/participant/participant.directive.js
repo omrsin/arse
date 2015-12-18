34bf25b5399122bd8d1dd39a01ace30ff13ee19c
Omar Erminy <omrsin@gmail.com>
2015-12-18 16:01:07 +0100

    Disables the button for removing the logged in user if he is a participant in a project

--- ARSE/client/app/directives/participant/participant.directive.js
+++ ARSE/client/app/directives/participant/participant.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('arseApp')
-  .directive('participant', function ($http, Modal, $stateParams) {
+  .directive('participant', function ($http, Modal, $stateParams, Auth, $cookies) {
     return {
       templateUrl: 'app/directives/participant/participant.html',
       restrict: 'E',
@@ -11,17 +11,17 @@ angular.module('arseApp')
         index: '@'        
       },
       link: function (scope, element, attrs) {
-        //scope.item = angular.fromJson(attrs.participantItem);        
+        scope.currentUser = {};
+        if($cookies.get('token')){
+          $http.get('api/users/me').then(function(res){
+            scope.currentUser = res.data;
+          }); 
+        }
 
         scope.deleteItem = function (item) {
           Modal.open({}, 'components/confirmModal/confirmModal.html', 'ConfirmModalCtrl', 
             { message: "Are you sure you want to delete participant "+item.user.username+"?"}).result.then(function (res) {  
               scope.remove({participant: item});
-
-            /*$http.delete('/api/projects/'+$stateParams.project_id+'/participants/'+scope.item.user._id).then(function(project){              
-              //scope.$emit('participantDeleted');
-              scope.project = project;
-            });*/
           });
         };
       }   
