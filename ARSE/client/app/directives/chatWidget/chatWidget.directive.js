a278a5b7d9d3865defc3c42b7aca87df3130ab21
Ilmi Ali <ilmiali@gmail.com>
2016-02-21 15:40:53 +0100

    Message indicator is done for both desktop and mobile

--- ARSE/client/app/directives/chatWidget/chatWidget.directive.js
+++ ARSE/client/app/directives/chatWidget/chatWidget.directive.js
@@ -6,46 +6,53 @@ angular.module('arseApp')
       templateUrl: 'app/directives/chatWidget/chatWidget.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
-        scope.isShownChat =  $rootScope.chatState;
+        scope.isShownChat = $rootScope.chatState;
         scope.message = "";
+        scope.counter = $rootScope.msgCounter;
 
-        
-        scope.enterMessage = function(evt){
 
-          if(evt.keyCode === 13) {
+        scope.enterMessage = function (evt) {
+
+          if (evt.keyCode === 13) {
             scope.sendMessage(scope.message);
           }
         };
-        
-        scope.showChati = function() {
+
+        scope.showChati = function () {
           scope.isShownChat = !scope.isShownChat;
           $rootScope.chatState = scope.isShownChat;
-        }
-
-//         $http.get('/api/projects/' + $stateParams.project_id).success(function (project) {
-          // scope.project = project;
-          scope.messageLog = scope.project.chat.slice(-10);
-//  
-//           // Update array with any new or deleted items pushed from the socket
-          socket.syncUpdates('project' + $stateParams.project_id, scope.project.chat, function (evt, msg, chat) {
-            console.log(chat.length - msg.length);
-            scope.$apply(function () {
-              scope.messageLog = msg.slice(-10);
-            });
-          });
-//         });
-
-        scope.sendMessage = function (message) {
-          Auth.getCurrentUser(function (user) {
-            console.log(user);
-            $http.put('/api/projects/' + $stateParams.project_id + '/post', { user: user.username, text: message }).success(function (res) {
-              console.log(res);
-              scope.message = '';
-            });
-
-          });
-
-        };
+          scope.counter = 0;
+          $rootScope.msgCounter = scope.counter;
       }
-    };
+
+        //         $http.get('/api/projects/' + $stateParams.project_id).success(function (project) {
+        // scope.project = project;
+        scope.messageLog = scope.project.chat.slice(-10);
+      //  
+      //           // Update array with any new or deleted items pushed from the socket
+      socket.syncUpdates('project' + $stateParams.project_id, scope.project.chat, function (evt, msg, chat) {
+        console.log(chat.length - msg.length);
+        scope.counter++;
+        $rootScope.msgCounter = scope.counter;
+        scope.$apply(function () {
+          scope.messageLog = msg.slice(-10);
+        });
+
+
+      });
+      //         });
+
+      scope.sendMessage = function (message) {
+        Auth.getCurrentUser(function (user) {
+          console.log(user);
+          $http.put('/api/projects/' + $stateParams.project_id + '/post', { user: user.username, text: message }).success(function (res) {
+            console.log(res);
+            scope.message = '';
+          });
+
+        });
+
+      };
+    }
+  };
   });
