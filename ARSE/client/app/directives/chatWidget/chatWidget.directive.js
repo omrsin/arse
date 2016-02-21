7a582777646a5750361c6d059948b9fb296a43aa
Ilmi Ali <ilmiali@gmail.com>
2016-02-21 12:41:28 +0100

    added scroll to the chat

--- ARSE/client/app/directives/chatWidget/chatWidget.directive.js
+++ ARSE/client/app/directives/chatWidget/chatWidget.directive.js
@@ -1,34 +1,41 @@
 'use strict';
 
 angular.module('arseApp')
-  .directive('chatWidget', function () {
+  .directive('chatWidget', function ($http, socket, $stateParams, Auth) {
     return {
       templateUrl: 'app/directives/chatWidget/chatWidget.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
         scope.isShown = false;
+        scope.message = "";
 
+        
+        scope.enterMessage = function(evt){
 
+          if(evt.keyCode === 13) {
+            scope.sendMessage(scope.message);
+          }
+        }
 
         $http.get('/api/projects/' + $stateParams.project_id).success(function (project) {
-          $scope.project = project;
-          $scope.messageLog = project.chat.slice(-10);
+          scope.project = project;
+          scope.messageLog = project.chat.slice(-10);
  
           // Update array with any new or deleted items pushed from the socket
-          socket.syncUpdates('project' + $stateParams.project_id, $scope.project.chat, function (evt, msg, chat) {
+          socket.syncUpdates('project' + $stateParams.project_id, scope.project.chat, function (evt, msg, chat) {
             console.log(chat.length - msg.length);
-            $scope.$apply(function () {
-              $scope.messageLog = msg.slice(-10);
+            scope.$apply(function () {
+              scope.messageLog = msg.slice(-10);
             });
           });
         });
 
-        $scope.sendMessage = function (message) {
+        scope.sendMessage = function (message) {
           Auth.getCurrentUser(function (user) {
             console.log(user);
             $http.put('/api/projects/' + $stateParams.project_id + '/post', { user: user.username, text: message }).success(function (res) {
               console.log(res);
-              $scope.message = '';
+              scope.message = '';
             });
 
           });
