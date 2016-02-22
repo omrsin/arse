1acc50601f2b05fc115910b5a35d98d490896c93
Ilmi Ali <ilmiali@gmail.com>
2016-02-22 13:49:39 +0100

    fixed when load more returns empty chat text

--- ARSE/client/app/directives/chatWidget/chatWidget.directive.js
+++ ARSE/client/app/directives/chatWidget/chatWidget.directive.js
@@ -4,7 +4,7 @@ angular.module('arseApp')
   .directive('chatWidget', function ($http, socket, $stateParams, Auth, $rootScope) {
     return {
       templateUrl: 'app/directives/chatWidget/chatWidget.html',
-      restrict: 'EA',
+      restrict: 'E',
       link: function (scope, element, attrs) {
         scope.isShownChat = $rootScope.chatState;
         scope.message = "";
@@ -35,15 +35,13 @@ angular.module('arseApp')
           $rootScope.msgCounter[scope.project._id] = scope.counter;
         }
 
-        //         $http.get('/api/projects/' + $stateParams.project_id).success(function (project) {
-        // scope.project = project;
         scope.messageLog = scope.project.chat.slice(-scope.loadLimit);
         //  
         //           // Update array with any new or deleted items pushed from the socket
-        socket.syncUpdates('project' + $stateParams.project_id, scope.project.chat, function (evt, msg, chat){ 
+        socket.syncUpdates('project' + $stateParams.project_id, scope.project.chat, function (evt, msg, chat) { 
 
           //handles project events that do not change the chat
-          if((msg.length - chat.length) === -1 || scope.difference == msg.length - chat.length) return;
+          if ((msg.length - chat.length) === -1 || scope.difference == msg.length - chat.length) return;
           scope.difference = msg.length - chat.length;
 
           scope.counter++;
@@ -58,18 +56,19 @@ angular.module('arseApp')
 
         scope.loadMore = function () {
           scope.loadLimit = scope.loadLimit + 10;
-          scope.messageLog = scope.project.chat.slice(-scope.loadLimit);
+          scope.messageLog = scope.fullChat.slice(-scope.loadLimit);
         }
 
         scope.sendMessage = function (message) {
-          Auth.getCurrentUser(function (user) {
-            console.log(user);
-            $http.put('/api/projects/' + $stateParams.project_id + '/post', { user: user.username, text: message }).success(function (res) {
-              console.log(res);
-              scope.message = '';
+          if (message !== '') {
+            Auth.getCurrentUser(function (user) {
+              console.log(user);
+              $http.put('/api/projects/' + $stateParams.project_id + '/post', { user: user.username, text: message }).success(function (res) {
+                console.log(res);
+                scope.message = '';
+              });
             });
-
-          });
+          }
 
         };
       }
