6f2fb5d711c714812202bb0474ee4acc42e225e3
Omar Erminy <omrsin@gmail.com>
2016-03-22 16:50:04 +0100

    First approaches to display the closed stories in a past sprint

--- ARSE/client/app/pastsprint/pastsprint.controller.js
+++ ARSE/client/app/pastsprint/pastsprint.controller.js
@@ -3,4 +3,18 @@
 angular.module('arseApp')
 	.controller('PastSprintCtrl', ['$scope', '$state', 'project', '$stateParams', function ($scope, $state, project, $stateParams) {
 		$scope.project = project;
+    $scope.showDetails = false;
+
+    $scope.showSprintDetails = function (item) {
+      // if ($scope.detailStory._id == item._id) {
+      //   $scope.detailStory = {};
+      //   $scope.showDetails = false;
+      // } else {
+      //   $scope.detailStory = item;
+      //   $scope.showDetails = true;
+      // }
+      console.log($scope.showDetails);
+      $scope.showDetails = !$scope.showDetails;
+    };
+
 	}]);
